aac3aa73ba571ddcc97465d7daa73572417905a5
Kytra Murphree <kytramurphree@kytras-mbp.attlocal.net>
2020-04-09 16:04:04 -0500

    Added express.static to serve client to localhost:5500.

--- client-server.js
+++ client-server.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('./public'));
+
 app.listen(5500, () => console.log('Client Server is Serving Index.html at http://localhost:5500'));
